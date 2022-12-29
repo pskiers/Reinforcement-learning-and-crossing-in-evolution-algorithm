2eb3f6ffdc0e37b4902c83ba9aa776ff3e58d025
Marcin <mlojek@protonmail.com>
2022-12-29 15:50:47 +0100

    remove obsolete stuff, leave only QLearningEvolution class

--- qlearning_evolution.py
+++ qlearning_evolution.py
@@ -3,16 +3,6 @@ Module containing implementation of qlearning algorithm
 """
 import numpy as np
 
-from evolution_algorithm import EvolutionAlgorithm, one_point_crossing, average_crossing, uniform_crossing
-
-
-crossing_functions = [one_point_crossing, average_crossing, uniform_crossing]
-crossing_probabilities = [(x*10 + 5)/100 for x in range(10)]
-
-# mean distance and success rate will be discreticised using np.digitize
-distance_bins = [10**x for x in range(6)]
-success_bins = [(x*10 + 5)/100 for x in range(10)]
-
 
 class QLearningEvolution:
     def __init__(self, mean_distance_bins: list, success_rate_bins: list, crossing_functions: list, crossing_probabilities: list, epsilon: float):
