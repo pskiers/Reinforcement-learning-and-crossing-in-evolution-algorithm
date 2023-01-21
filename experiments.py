24464d04eea1284ec058edcf40b0561d62ffa24d
Marcin <mlojek@protonmail.com>
2023-01-21 09:05:16 +0100

    fixes to experiments scripts

--- experiments.py
+++ experiments.py
@@ -1,7 +1,8 @@
+import numpy as np
 import pandas as pd
 
 from cec2017.functions import f21, f22, f23, f24
-from qlearning_evolution import QLearningEvolution
+from qlearning_evolution import QLearningEvolution, Record
 from evolution_algorithm import EvolutionAlgorithm
 from evolution_algorithm import one_point_crossing, average_crossing, uniform_crossing
 
@@ -15,11 +16,11 @@ VALUE_FUNCTIONS = {'f21': f21, 'f22': f22, 'f23': f23, 'f24': f24}
 # constant hyperparams
 EPOCHS = 0
 MEAN_DISTANCE_BINS = [10**n for n in [2, 3, 4, 5, 6]]
-SUCCESS_RATE_BINS = [n/10 for n in float(range(10))]
+SUCCESS_RATE_BINS = np.arange(0.0, 1.0, 0.1)
 CROSSING_FUNCTIONS = {'one_point_crossing': one_point_crossing,
                       'average_crossing': average_crossing,
                       'uniform_crossing': uniform_crossing}
-CROSSING_PROBABILITIES = [n/10 for n in float(range(1, 10))]
+CROSSING_PROBABILITIES = np.arange(0.1, 1.0, 0.1)
 EPSILON = 0.2
 LEARNING_RATE = 0.9
 DISCOUNT_FACTOR = 0.5
@@ -32,24 +33,32 @@ OUTPUT_FILE = 'results.csv'
 
 
 if __name__ == '__main__':
-    df = pd.DataFrame(['population_size',
-                       'mutation_strength',
-                       'dimensionalities',
-                       'value_function',
-                       'epochs',
-                       'mean_distances',
-                       'success_rate_bins',
-                       'crossing_functions',
-                       'crossing_probabilities',
-                       'epsilon',
-                       'learning_rate',
-                       'discount_factor'])
+    df = pd.DataFrame(columns = [
+                      'population_size',
+                      'mutation_strength',
+                      'dimensionalities',
+                      'value_function',
+                      'epochs',
+                      'mean_distances',
+                      'success_rate_bins',
+                      'crossing_functions',
+                      'crossing_probabilities',
+                      'epsilon',
+                      'learning_rate',
+                      'discount_factor'])
 
     for pop_size in POPULATION_SIZES:
         for mut_strength in MUTATION_STRENGTHS:
             for dim in DIMENSIONALITIES:
-                for func in VALUE_FUNCTIONS.keys():
-                    # run QLE
+                for func_name in VALUE_FUNCTIONS.keys():
+                    ea = EvolutionAlgorithm(value_function=VALUE_FUNCTIONS[func_name],
+                                            population_size=pop_size,
+                                            mutation_strength=mut_strength,
+                                            upper_bound=100,
+                                            dimensionality=dim)
+                    # qle = QLearningEvolution()
+                    # run QLE 25 times
+                    # collect records
                     # save results to dataframe
                     pass
 
